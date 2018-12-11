36a558420e7179866e79202490c6886b60287ded
Henry Townley <htownley@me.com>
2018-12-11 02:33:36 -0500

    Update usage cap

--- application.py
+++ application.py
@@ -159,8 +159,8 @@ def energyovertime():
 			'lat':float(thing[1]),
 			'lng':float(thing[2]),
 			'id':float(thing[0]),
-			'usage':[(float(thing[3]) if float(thing[3]) < 100000 else 100000)],
-			'sqftusage':[(float(thing[4]) if float(thing[4]) < 100000 else 100000)]
+			'usage':[(float(thing[3]) if float(thing[3]) < 100 else 0)],
+			'sqftusage':[(float(thing[4]) if float(thing[4]) < 100 else 0)]
 		}
 
 		if thing['id'] in all_buildings:
