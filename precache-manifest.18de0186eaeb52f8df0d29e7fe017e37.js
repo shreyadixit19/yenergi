fc7d99d9e61c3f1d0695d7a91d9b50d4046acaf9
Jo-Jo Feng <yanlin.feng@yale.edu>
2018-12-11 22:47:38 -0500

    Updates

---
+++ precache-manifest.18de0186eaeb52f8df0d29e7fe017e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1375348d64b12d33f97",
+    "url": "/yenergi/static/js/runtime~main.c1375348.js"
+  },
+  {
+    "revision": "24e5b6a6297e2c70a06e",
+    "url": "/yenergi/static/js/main.24e5b6a6.chunk.js"
+  },
+  {
+    "revision": "65ea75562610ade22fa3",
+    "url": "/yenergi/static/js/1.65ea7556.chunk.js"
+  },
+  {
+    "revision": "24e5b6a6297e2c70a06e",
+    "url": "/yenergi/static/css/main.9b2c4835.chunk.css"
+  },
+  {
+    "revision": "736593ef11379282b9993891d5e598b9",
+    "url": "/yenergi/index.html"
+  }
+];
